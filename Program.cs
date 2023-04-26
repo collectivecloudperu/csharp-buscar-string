a8e3eb9af7d3e56b8ab75c0767bd84c84eee2fa1
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2023-04-25 19:05:27 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace buscarTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string texto = "Crack de la Programación !";
+            string textoabuscar = "Zapato"; 
+
+            if(texto.Contains(textoabuscar)){
+                Console.WriteLine("Si existe");
+            }
+            else {
+                Console.WriteLine("No existe");
+            }
+
+        }
+    }
+}
